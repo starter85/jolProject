31e5a8830b23b35e02024621aa2d0353a457c5b9
starter85 <beforel98@hanmail.net>
2022-05-28 23:57:12 +0900

    첫번째

---
+++ GoCook_/app/src/main/java/com/example/gocook_/UserThingActivity.java
@@ -0,0 +1,14 @@
+package com.example.gocook_;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserThingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_thing); //xml , java 소스 연결
+    }
+}
